5129645cf97ecdf9d11f054e93f93ae19bdb328f
juhyeong park <majecty@gmail.com>
2014-10-12 17:35:36 +0900

    Make ask to title run.

---
+++ Assets/Script/UI/RestartScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartScene : MonoBehaviour {
+
+	void OnMouseDown()
+	{
+		StartCoroutine(load_game_after_delay());
+	}
+	
+	IEnumerator load_game_after_delay()
+	{
+		yield return new WaitForSeconds(1);
+		Application.LoadLevel(Application.loadedLevelName);
+	}
+}
